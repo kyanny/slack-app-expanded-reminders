a0bfa759cdfc819f683d5dcdf975f8422fcdabc4
Kensuke Nagae <kyanny@gmail.com>
2018-01-28 04:14:24 +0900

    sample button

--- server.rb
+++ server.rb
@@ -9,6 +9,20 @@ post '/' do
 
   content_type 'application/json'
   {
-    "text": "New comic book alert! _The Further Adventures of Slackbot_, Volume 1, Issue 3."
-  }.to_json
+  "text": "<@W1A2BC3DD> approved your travel request. Book any airline you like by continuing below.",
+  "channel": "C061EG9SL",
+  "attachments": [
+    {
+      "fallback": "Book your flights at https://flights.example.com/book/r123456",
+      "actions": [
+        {
+          "type": "button",
+          "text": "Book flights 🛫",
+          "url": "https://flights.example.com/book/r123456"
+        }
+      ]
+    }
+  ]
+}.to_json
+  
 end
