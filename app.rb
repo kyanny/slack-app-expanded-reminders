ff9c2cddab376991a9b12966212a0efa0c0fc48b
Kensuke Nagae <kyanny@gmail.com>
2018-01-28 04:33:08 +0900

    fix

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App
 
     def get_expanded_reminders
         reminders = client.reminders_list.reminders
-        reminders.reject { |reminder|
+        reminders = reminders.reject { |reminder|
             reminder.complete_ts != 0
         }.map { |reminder|
             if m = reminder.text.match(%r!\Ahttps://quipper\.slack\.com/archives/([^/]*)/p([^/]*)\?thread_ts=(.*)&cid=(.*)\z!)
@@ -23,30 +23,30 @@ class App
                 thread_ts = m[3].to_f / 1000000
                 res = client.conversations_replies(channel: channel, inclusive: true, ts: thread_ts, latest: ts, limit: 1)
                 text = res.messages.last.text
-                format reminder.text, text
+                [reminder.text, text]
             elsif m = reminder.text.match(%r!\Ahttps://quipper\.slack\.com/archives/([^/]*)/p([^/]*)\z!)
                 channel = m[1]
                 ts      = m[2].to_f / 1000000
                 res = client.conversations_history(channel: channel, inclusive: true, latest: ts+0.000001, limit: 1)
                 if res.messages.first.subtype == 'file_share'
                     text = res.messages.first.file.preview
-                    format reminder.text, text
+                    [reminder.text, text]
                 else
                     text = res.messages.first.text
-                    format reminder.text, text
+                    [reminder.text, text]
                 end
             end
         }
-    end
 
-    def format(text, message)
         {
-            text: "<#{text}|message> #{message}",
-            attachments: [
+            text: "Expanded reminders",
+            attachments: reminders.map { |(text, message)|
+                msg = "#{text} #{message}"
                 {
                     actions: [
                         {
-                            fallback: "Complete",
+                            text: msg,
+                            fallback: msg,
                             actions: {
                                 name: "complete",
                                 text: "Complete",
@@ -56,7 +56,7 @@ class App
                         }
                     ]
                 }
-            ]
+            }
         }
     end
 end
