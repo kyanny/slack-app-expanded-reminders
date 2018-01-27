2636f9c485f292a4fe5f5c32e448a1fa4a8e023c
Kensuke Nagae <kyanny@gmail.com>
2018-01-28 04:17:58 +0900

    another sample

--- server.rb
+++ server.rb
@@ -8,21 +8,44 @@ post '/' do
   app.get_expanded_reminders.to_s
 
   content_type 'application/json'
+
   {
-  "text": "<@W1A2BC3DD> approved your travel request. Book any airline you like by continuing below.",
-  "channel": "C061EG9SL",
-  "attachments": [
-    {
-      "fallback": "Book your flights at https://flights.example.com/book/r123456",
-      "actions": [
+    "text": "Would you like to play a game?",
+    "attachments": [
         {
-          "type": "button",
-          "text": "Book flights 🛫",
-          "url": "https://flights.example.com/book/r123456"
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
         }
-      ]
-    }
-  ]
-}.to_json
-  
+    ]
+  }.to_json 
 end
