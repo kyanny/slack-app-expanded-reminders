a88fecc70b6e0a5ea523abcf39f572b53d821ca4
Kensuke Nagae <kyanny@gmail.com>
2018-01-28 04:11:27 +0900

    try to_json

--- server.rb
+++ server.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require_relative 'app'
+require 'json'
 
 post '/' do
   #'Hello from Expanded reminders'
   app = App.new
   app.get_expanded_reminders.to_s
+
+  {
+    "text": "New comic book alert! _The Further Adventures of Slackbot_, Volume 1, Issue 3."
+  }.to_json
 end
