f7ef2db43359373a6a6b58a22cdcbc60fe458938
Kensuke Nagae <kyanny@gmail.com>
2018-01-28 04:04:23 +0900

    Implement slack app

---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'slack-ruby-client'
+require 'pp'
+
+Slack.configure do |config|
+  config.token = ENV.fetch('SLACK_API_TOKEN')
+end
+
+class App
+    def initialize
+        @client = Slack::Web::Client.new
+    end
+    
+    attr_reader :client
+
+    def get_expanded_reminders
+        reminders = client.reminders_list.reminders
+        reminders.reject { |reminder|
+            reminder.complete_ts != 0
+        }.map { |reminder|
+            if m = reminder.text.match(%r!\Ahttps://quipper\.slack\.com/archives/([^/]*)/p([^/]*)\?thread_ts=(.*)&cid=(.*)\z!)
+                channel   = m[1]
+                ts        = m[2].to_f / 1000000
+                thread_ts = m[3].to_f / 1000000
+                res = client.conversations_replies(channel: channel, inclusive: true, ts: thread_ts, latest: ts, limit: 1)
+                text = res.messages.last.text
+                [reminder.text, text]
+            elsif m = reminder.text.match(%r!\Ahttps://quipper\.slack\.com/archives/([^/]*)/p([^/]*)\z!)
+                channel = m[1]
+                ts      = m[2].to_f / 1000000
+                res = client.conversations_history(channel: channel, inclusive: true, latest: ts+0.000001, limit: 1)
+                if res.messages.first.subtype == 'file_share'
+                    text = res.messages.first.file.preview
+                    [reminder.text, text]
+                else
+                    text = res.messages.first.text
+                    [reminder.text, text]
+                end
+            end
+        }
+    end
+end
+
+#pp App.new.get_expanded_reminders
--- server.rb
+++ server.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
+require_relative 'app'
 
 post '/' do
-  'Hello from Expanded reminders'
+  #'Hello from Expanded reminders'
+  app = App.new
+  app.get_expanded_reminders
 end
